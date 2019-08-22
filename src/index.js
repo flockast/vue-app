58614bf4a8a94ba157bddbd5c8e4af0693a43392
konstantin vlasov <k.vlasov@game-insight.com>
2019-08-22 17:40:06 +0300

    included bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles/index.scss";
 
 import Vue from "vue";
-import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
 import router from "./router";
 import store from "./store";
+import App from "./App.vue";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
