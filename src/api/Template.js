1b329c203a9aac21cf2788ff77206dffc06d7224
konstantin vlasov <k.vlasov@game-insight.com>
2019-08-31 03:43:50 +0300

    post payload like a string

--- src/api/Template.js
+++ src/api/Template.js
@@ -2,7 +2,7 @@ import Service from './Service';
 
 export default {
   getAll () {
-    return Service.post('content.templates?all=1', {}, (status, data) => {
+    return Service.post('content.templates', 'all=1', (status, data) => {
       return Object.keys(data.data).map(key => data.data[key]);
     });
   }
