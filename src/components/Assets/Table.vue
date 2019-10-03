afd1c9eb1d7206e1be1a3c35941d5092b8419d31
konstantin vlasov <k.vlasov@game-insight.com>
2019-10-03 17:07:35 +0300

    added filter for title param

--- src/components/Assets/Table.vue
+++ src/components/Assets/Table.vue
@@ -1,44 +1,55 @@
 <template>
-  <div class="table-wrapper">
-    <table class="table">
-      <thead>
-        <tr>
-          <th></th>
-          <th>id</th>
-          <th v-for="(param, index) in template.params" :key="index">{{ param.title }}</th>
-        </tr>
-      </thead>
-      <Tr v-for="asset in assets"
-          :template="template"
-          :link-assets="linkAssets"
-          :asset="asset"
-          :key="asset.id"/>
-      <Tr v-for="asset in newAssets"
-          :template="template"
-          :link-assets="linkAssets"
-          :asset="asset"
-          :keyOfNewAsset="asset.newKey"
-          @removeFromNewAsset="removeFromNewAsset"
-          :key="asset.newKey"/>
-      <tbody>
-        <tr>
-          <td :colspan="template.params.length + 2">
-            <button class="button button--full button--success" @click="handleClickAddRow"><i class="fas fa-plus"></i> Добавить</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <div class="table-wrapper">
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>id</th>
+            <th v-for="(param, index) in template.params" :key="index">
+              <span v-if="param.id === 'title'">
+                <div class="table-filter">
+                  <input type="text" class="input" placeholder="Фильтр по названию" v-model="filterQuery">
+                </div>
+              </span>
+              <span v-else>{{ param.title }}</span>
+            </th>
+          </tr>
+        </thead>
+        <Tr v-for="asset in filteredAssets"
+            :template="template"
+            :link-assets="linkAssets"
+            :asset="asset"
+            :key="asset.id"/>
+        <Tr v-for="asset in newAssets"
+            :template="template"
+            :link-assets="linkAssets"
+            :asset="asset"
+            :keyOfNewAsset="asset.newKey"
+            @removeFromNewAsset="removeFromNewAsset"
+            :key="asset.newKey"/>
+        <tbody>
+          <tr>
+            <td :colspan="template.params.length + 2">
+              <button class="button button--full button--success" @click="handleClickAddRow"><i class="fas fa-plus"></i> Добавить</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import helpers from '../../mixins/helpers';
 import Tr from './TableTr';
 
 export default {
   data () {
     return {
       newAssets: [],
-      keyOfNewAsset: 0
+      keyOfNewAsset: 0,
+      filterQuery: ''
     };
   },
   props: {
@@ -46,6 +57,16 @@ export default {
     linkAssets: [ Array ],
     assets: [ Array ]
   },
+  computed: {
+    filteredAssets () {
+      return this.assets.filter(item =>
+        item.values && item.values.title
+          ? this.readyToSearch(item.values.title).includes(this.readyToSearch(this.filterQuery))
+          : this.assets
+      );
+    }
+  },
+  mixins: [ helpers ],
   methods: {
     handleClickAddRow () {
       let data = {
@@ -65,6 +86,7 @@ export default {
   watch: {
     template () {
       this.newAssets = [];
+      this.filterQuery = '';
     }
   },
   components: {
